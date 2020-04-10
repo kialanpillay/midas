3d966e7fe6c201cc592820a1076a55f5093c7724
Kialan Pillay <kialanpillay@gmail.com>
2020-04-10 19:52:34 +0200

    NewsAPI Integration

--- server/midas.py
+++ server/midas.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from flask_restplus import Api, Resource, fields
+import requests
 # from sklearn.externals import joblib
 
 app = Flask(__name__)
@@ -29,6 +30,9 @@ model = api.model('Midas Params',
 ###################
 # Sentiment Analysis
 
+url = "https://newsapi.org/v2/everything?q=(gold AND commodity) OR XAUUSD (gold AND prices) &apiKey=e3c7d810af0e41dd869013ab5c5d66e9"
+# r = requests.get(url = url) 
+# data = r.json() 
 
 
 
