7c0cc6c0665dbde5d0e733227e4267b8fbeea475
Kialan Pillay <kialanpillay@gmail.com>
2020-04-11 18:50:17 +0200

    Implementation Changes
    Model Training - Server Start
    Classification - API Call

--- server/midas.py
+++ server/midas.py
@@ -22,24 +22,8 @@ model = api.model(
 )
 
 ###################
-# Sentiment Analysis
+# Sentiment Analysis - Model Training
 
-url = "https://newsapi.org/v2/everything?q=(gold AND commodity) OR XAUUSD&apiKey=e3c7d810af0e41dd869013ab5c5d66e9"
-r = requests.get(url=url)
-data = r.json()
-results = data["totalResults"]
-
-pages = int(100/20)
-if(results%20!=0):
-    pages += 1
-
-articles = []
-for i in range(1,pages):
-    url = "https://newsapi.org/v2/everything?q=(gold AND commodity) OR XAUUSD&page="+str(i)+"&apiKey=e3c7d810af0e41dd869013ab5c5d66e9"
-    r = requests.get(url=url)
-    data = r.json()
-    print(data)
-    articles.append(data['articles'])
 
 def remove_noise(tweet_tokens, stop_words=()):
 
@@ -84,87 +68,123 @@ def get_tweets_for_model(cleaned_tokens_list):
         yield dict([token, True] for token in tweet_tokens)
 
 
-positive_tweets = twitter_samples.strings("positive_tweets.json")
-negative_tweets = twitter_samples.strings("negative_tweets.json")
+def sentiment_model_train():
+	positive_tweets = twitter_samples.strings("positive_tweets.json")
+	negative_tweets = twitter_samples.strings("negative_tweets.json")
 
-stop_words = stopwords.words("english")
+	stop_words = stopwords.words("english")
 
-positive_tweet_tokens = twitter_samples.tokenized("positive_tweets.json")
-negative_tweet_tokens = twitter_samples.tokenized("negative_tweets.json")
+	positive_tweet_tokens = twitter_samples.tokenized("positive_tweets.json")
+	negative_tweet_tokens = twitter_samples.tokenized("negative_tweets.json")
 
-positive_cleaned_tokens_list = []
-negative_cleaned_tokens_list = []
+	positive_cleaned_tokens_list = []
+	negative_cleaned_tokens_list = []
 
-for tokens in positive_tweet_tokens:
-    positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+	for tokens in positive_tweet_tokens:
+		positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-for tokens in negative_tweet_tokens:
-    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+	for tokens in negative_tweet_tokens:
+		negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-all_pos_words = get_all_words(positive_cleaned_tokens_list)
+	all_pos_words = get_all_words(positive_cleaned_tokens_list)
 
-freq_dist_pos = FreqDist(all_pos_words)
+	freq_dist_pos = FreqDist(all_pos_words)
 
-positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
-negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
+	positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
+	negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
 
-positive_dataset = [
-    (tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model
-]
+	positive_dataset = [
+		(tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model
+	]
 
-negative_dataset = [
-    (tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model
-]
+	negative_dataset = [
+		(tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model
+	]
 
-dataset = positive_dataset + negative_dataset
+	dataset = positive_dataset + negative_dataset
 
-train_data = dataset
+	train_data = dataset
 
-classifier = NaiveBayesClassifier.train(train_data)
+	classifier = NaiveBayesClassifier.train(train_data)
+	return classifier
 
-positive_articles = 0
-negative_articles = 0
+def sentiment(classifier):
 
-print(len(data['articles']))
-for article in data['articles']:
-    description = article['description']
-    description_tokens = remove_noise(word_tokenize(description))
-    classification = classifier.classify(dict([token, True] for token in description_tokens))
-    if classification == "Positive":
-        positive_articles +=1
-    else:
-        negative_articles +=1
+    url = "https://newsapi.org/v2/everything?q=(gold AND commodity) OR XAUUSD&apiKey=e3c7d810af0e41dd869013ab5c5d66e9"
+    r = requests.get(url=url)
+    data = r.json()
+    results = data["totalResults"]
+
+    pages = int(100 / 20)
+    if results % 20 != 0:
+        pages += 1
+
+    articles = []
+    for i in range(1, pages):
+        url = (
+            "https://newsapi.org/v2/everything?q=(gold AND commodity) OR XAUUSD&page="
+            + str(i)
+            + "&apiKey=e3c7d810af0e41dd869013ab5c5d66e9"
+        )
+        r = requests.get(url=url)
+        data = r.json()
+        articles.append(data["articles"])
+
+    
+
+    positive_articles = 0
+    negative_articles = 0
+    neutral_articles = 0
+
+    for article in articles:
+        description = article["description"]
+        description_tokens = remove_noise(word_tokenize(description))
+        classification = classifier.classify(
+            dict([token, True] for token in description_tokens)
+        )
+        if classification == "Positive":
+            positive_articles += 1
+        else:
+            negative_articles += 1
+
+    return results, positive_articles, negative_articles, neutral_articles
+
 
 ###################
+classifier = sentiment_model_train()
+print("Sentiment Classifier Trained")
+###################
+
 
 
 @api.route("/midas/sentiment")
 class Sentiment(Resource):
-    def options(self):
-        response = make_response()
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        response.headers.add("Access-Control-Allow-Headers", "*")
-        response.headers.add("Access-Control-Allow-Methods", "*")
-        return response
+	def options(self):
+		response = make_response()
+		response.headers.add("Access-Control-Allow-Origin", "*")
+		response.headers.add("Access-Control-Allow-Headers", "*")
+		response.headers.add("Access-Control-Allow-Methods", "*")
+		return response
 
-    @api.expect(model)
-    def get(self):
-        try:
-            # formData = request.json
-            response = jsonify(
+	@api.expect(model)
+	def get(self):
+		try:
+			results, positive_articles, negative_articles, neutral_articles = sentiment(classifier)
+			response = jsonify(
                 {
                     "statusCode": 200,
                     "status": "Successful",
-                    "positive": (positive_articles/20),
-                    "negative": (negative_articles/20),
-                    "neutral": 0,
+                    "positive": int((positive_articles / 100) * 100),
+                    "negative": int((negative_articles / 100) * 100),
+                    "neutral": int((neutral_articles / 100) * 100),
                     "articles": results,
                 }
             )
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
-        except Exception as error:
-            return jsonify({"statusCode": 500, "status": "Error", "error": str(error)})
+			response.headers.add("Access-Control-Allow-Origin", "*")
+			return response
+		except Exception as error:
+			return jsonify({"statusCode": 500, "status": "Error", "error": str(error)})
+
 
 
 if __name__ == "__main__":
