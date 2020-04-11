ae723690f561f7d5ab231e91408df0a752bd2008
Kialan Pillay <kialanpillay@gmail.com>
2020-04-11 17:52:19 +0200

    Refactor

--- server/sentiment.py
+++ server/sentiment.py
@@ -1,3 +1,22 @@
+import requests
+
+
+url = "https://newsapi.org/v2/everything?q=(gold AND commodity) OR XAUUSD&apiKey=e3c7d810af0e41dd869013ab5c5d66e9"
+r = requests.get(url=url)
+data = r.json()
+results = data["totalResults"]
+
+pages = int(100/20)
+if(results%20!=0):
+    pages += 1
+articles = []
+for i in range(1,pages):
+    url = "https://newsapi.org/v2/everything?q=(gold AND commodity) OR XAUUSD&page="+str(i)+"&apiKey=e3c7d810af0e41dd869013ab5c5d66e9"
+    r = requests.get(url=url)
+    data = r.json()
+    print(data)
+    articles.append(data['articles'])
+
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import twitter_samples, stopwords
 from nltk.tag import pos_tag
@@ -50,60 +69,60 @@ def get_tweets_for_model(cleaned_tokens_list):
         yield dict([token, True] for token in tweet_tokens)
 
 
-if __name__ == "__main__":
+positive_tweets = twitter_samples.strings("positive_tweets.json")
+negative_tweets = twitter_samples.strings("negative_tweets.json")
 
-    positive_tweets = twitter_samples.strings("positive_tweets.json")
-    negative_tweets = twitter_samples.strings("negative_tweets.json")
+stop_words = stopwords.words("english")
 
-    stop_words = stopwords.words("english")
+positive_tweet_tokens = twitter_samples.tokenized("positive_tweets.json")
+negative_tweet_tokens = twitter_samples.tokenized("negative_tweets.json")
 
-    positive_tweet_tokens = twitter_samples.tokenized("positive_tweets.json")
-    negative_tweet_tokens = twitter_samples.tokenized("negative_tweets.json")
+positive_cleaned_tokens_list = []
+negative_cleaned_tokens_list = []
 
-    positive_cleaned_tokens_list = []
-    negative_cleaned_tokens_list = []
+for tokens in positive_tweet_tokens:
+    positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-    for tokens in positive_tweet_tokens:
-        positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+for tokens in negative_tweet_tokens:
+    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-    for tokens in negative_tweet_tokens:
-        negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+all_pos_words = get_all_words(positive_cleaned_tokens_list)
 
-    all_pos_words = get_all_words(positive_cleaned_tokens_list)
+freq_dist_pos = FreqDist(all_pos_words)
 
-    freq_dist_pos = FreqDist(all_pos_words)
-    print(freq_dist_pos.most_common(10))
+positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
+negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
 
-    positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
-    negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
+positive_dataset = [
+    (tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model
+]
 
-    positive_dataset = [
-        (tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model
-    ]
+negative_dataset = [
+    (tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model
+]
 
-    negative_dataset = [
-        (tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model
-    ]
+dataset = positive_dataset + negative_dataset
 
-    dataset = positive_dataset + negative_dataset
+train_data = dataset
 
-    random.shuffle(dataset)
+classifier = NaiveBayesClassifier.train(train_data)
 
-    train_data = dataset[:7000]
-    test_data = dataset[3000:]
-
-    classifier = NaiveBayesClassifier.train(train_data)
-
-    print("Accuracy is:", classify.accuracy(classifier, test_data))
-
-    # print(classifier.show_most_informative_features(10))
-
-    custom_tweet = "Gold prices snapped a five session decline and traded positive after various measure undertaken by the Fed with an objective to stabilize the economy amidst the coronavirus outbreak."
-
-    custom_tokens = remove_noise(word_tokenize(custom_tweet))
-
-    print(
-        custom_tweet,
-        classifier.classify(dict([token, True] for token in custom_tokens)),
-    )
-
+positive_articles = 0
+negative_articles = 0
+articles = data["totalResults"]
+counter = 0
+print(len(data['articles']))
+for article in data['articles']:
+    description = article['description']
+    description_tokens = remove_noise(word_tokenize(description))
+    classification = classifier.classify(dict([token, True] for token in description_tokens))
+    print(classification)
+    counter += 1
+    if classification == "Positive":
+        positive_articles +=1
+    else:
+        negative_articles +=1
+    
+results = counter
+print(positive_articles/results)
+print(negative_articles/results)
