2ec4c77d1668900687aede152d54585e07acf469
Kialan Pillay <kialanpillay@gmail.com>
2020-04-11 17:52:35 +0200

    Refactor

--- server/news.py
+++ server/news.py
@@ -2,8 +2,4 @@ import requests
 
 url = "https://newsapi.org/v2/everything?q=(gold AND commodity) OR XAUUSD&apiKey=e3c7d810af0e41dd869013ab5c5d66e9"
 r = requests.get(url = url) 
-data = r.json() 
-
-for article in data["articles"]:
-    print(article['description'])
-    description_tokens = remove_noise(word_tokenize(description))
+data = r.json()
